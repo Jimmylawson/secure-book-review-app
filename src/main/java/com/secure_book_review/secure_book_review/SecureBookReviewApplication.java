391cb6118642e355026b234d5117df696e9d9781
jimmy <jimoseipeace2bb@gmail.com>
2025-03-28 12:39:50 -0500

    Created the starter dependencies. Close #1

---
+++ src/main/java/com/secure_book_review/secure_book_review/SecureBookReviewApplication.java
@@ -0,0 +1,13 @@
+package com.secure_book_review.secure_book_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureBookReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureBookReviewApplication.class, args);
+	}
+
+}
